68dbc9100cef629c97691c42938d197485752b05
Julien Elinckx <julienelinckx@hotmail.com>
2022-07-28 10:35:41 +0200

    MàJ 28/07/10h35

--- assets/scripts/shopping_cart.js
+++ assets/scripts/shopping_cart.js
@@ -5,51 +5,52 @@
 //     })
 // }
 
-function LignePanier (nom, qte, prix)
-{
-    this.nomArticle = nom;
-    this.qteArticle = qte;
-    this.prixArticle = prix;
-    this.ajouterQte = function(qte)
-    {
-        this.qteArticle += qte;
-    }
-    this.getPrixLigne = function()
-    {
-        let resultat = this.prixArticle * this.qteArticle;
-        return resultat;
-    }
-    this.getNom = function() 
-    {
-        return this.nomArticle;
-    }
-}
+// function LignePanier (nom, qte, prix)
+// {
+//     this.nomArticle = nom;
+//     this.qteArticle = qte;
+//     this.prixArticle = prix;
+//     this.ajouterQte = function(qte)
+//     {
+//         this.qteArticle += qte;
+//     }
+//     this.getPrixLigne = function()
+//     {
+//         let resultat = this.prixArticle * this.qteArticle;
+//         return resultat;
+//     }
+//     this.getNom = function() 
+//     {
+//         return this.nomArticle;
+//     }
+// }
 
-function Panier()
-{
-    this.liste = [];
-    this.ajouterArticle = function(nom, qte, prix)
-    { 
-        let index = this.getArticle(nom);
-        if (index == -1) this.liste.push(new LignePanier(nom, qte, prix));
-        else this.liste[index].ajouterQte(qte);
-    }
-    this.getPrixPanier = function()
-    {
-        let total = 0;
-        for(let i = 0 ; i < this.liste.length ; i++)
-            total += this.liste[i].getPrixLigne();
-        return total;
-    }
-    this.getArticle = function(nom)
-    {
-        for(let i = 0 ; i <this.liste.length ; i++)
-            if (nom == this.liste[i].getCode()) return i;
-        return -1;
-    }
-    this.supprimerArticle = function(nom)
-    {
-        let index = this.getArticle(nom);
-        if (index > -1) this.liste.splice(index, 1);
-    }
-}
+// function Panier()
+// {
+//     this.liste = [];
+//     this.ajouterArticle = function(nom, qte, prix)
+//     { 
+//         let index = this.getArticle(nom);
+//         if (index == -1) this.liste.push(new LignePanier(nom, qte, prix));
+//         else this.liste[index].ajouterQte(qte);
+//     }
+//     this.getPrixPanier = function()
+//     {
+//         let total = 0;
+//         for(let i = 0 ; i < this.liste.length ; i++)
+//             total += this.liste[i].getPrixLigne();
+//         return total;
+//     }
+//     this.getArticle = function(nom)
+//     {
+//         for(let i = 0 ; i <this.liste.length ; i++)
+//             if (nom == this.liste[i].getCode()) return i;
+//         return -1;
+//     }
+//     this.supprimerArticle = function(nom)
+//     {
+//         let index = this.getArticle(nom);
+//         if (index > -1) this.liste.splice(index, 1);
+//     }
+// }
+
