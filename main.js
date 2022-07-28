6c2edeb7d4881033dae38ffc3027c3807499bbbf
FriquetLuca <friquet.luca@gmail.com>
2022-07-28 16:08:32 +0200

    Dark mode btn logo switch

--- main.js
+++ main.js
@@ -163,14 +163,19 @@ for(let filterBtn of filterButtons)
 function toggleTheme()
 {
     let theme = document.getElementsByTagName('link')[1];
+    let currentBtn = document.getElementById('dark__theme');
     if (theme.getAttribute('href') == './assets/css/style-light-theme.css')
     {
         theme.setAttribute('href', './assets/css/style-dark-theme.css');
+        currentBtn.setAttribute('src', 'assets/img/moon.png');
+
     }
     else
     {
         theme.setAttribute('href', './assets/css/style-light-theme.css');
+        currentBtn.setAttribute('src', 'assets/img/sun.png');
     }
+    console.log(element);
 }
 
 
