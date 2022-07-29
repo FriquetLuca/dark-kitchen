1bec85e622ddd9dfaef03b8636b7db96c8d6c587
Julien Scourneau <scourneaujulien@outlook.com>
2022-07-29 11:45:12 +0200

    Added event to show or hide shopping cart

--- assets/scripts/panier.js
+++ assets/scripts/panier.js
@@ -1,32 +1,12 @@
-function LignePanier (qte, prix)
-{
-    this.qteArticle = qte;
-    this.prixArticle = prix;
-    this.ajouterQte = function(qte)
-    {
-        this.qteArticle += qte;
+const clickOnShoppingCart =(e) =>{
+    let shoppingCart = document.getElementsByClassName('container')
+    if(shoppingCart[0].style.display == "none"){
+        shoppingCart[0].style.display = "inline"
+    } else {
+        shoppingCart[0].style.display = "none"
     }
-    this.getPrixLigne = function()
-    {
-        var resultat = this.prixArticle * this.qteArticle;
-        return resultat;
-    }
+    
 }
 
-function Panier()
-{
-    this.liste = [];
-    this.ajouterArticle = function(qte, prix)
-    { 
-        let index = this.getArticle(code);
-        if (index == -1) this.liste.push(new LignePanier(qte, prix));
-        else this.liste[index].ajouterQte(qte);
-    }
-    this.getPrixPanier = function()
-    {
-        let total = 0;
-        for(let i = 0 ; i < this.liste.length ; i++)
-            total += this.liste[i].getPrixLigne();
-        return total;
-    }
-}
+const displayShoppingCart = document.querySelector('nav').lastElementChild.firstElementChild
+displayShoppingCart.addEventListener('click', clickOnShoppingCart)
